ee6a85d0ba0a2e19e0c8b0dd81b6a98c6ae5fd46
Anderson Souza <andersonpds14@gmail.com>
2019-11-30 17:26:15 -0300

    Deletado classe de testes
    Modificado Services para fazer a ponte entre as APIs
    APIConsumer agora retorna uma lista de Profissionais no método consumir

--- backend/src/main/java/com/elotech/client/consumer/ProfissionalAPIConsumer.java
+++ backend/src/main/java/com/elotech/client/consumer/ProfissionalAPIConsumer.java
@@ -1,12 +1,10 @@
 package com.elotech.client.consumer;
 
 import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Arrays;
+import java.util.List;
 
 import com.elotech.model.Profissional;
-import com.elotech.service.ProfissionalService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -14,50 +12,33 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-/**
- * Classe utilizada para consumir os recursos da API de profissionais e grava os
- * registros em um banco de dados local
- * 
- * @author ander
- *
- */
 public class ProfissionalAPIConsumer {
 
 	private final String RESOURCEURL = "https://homologacao.saude.dev.elotech.com.br/saude-api/api/profissional";
 	private OkHttpClient client;
 	private ObjectMapper objectMapper;
 
-	@Autowired
-	private ProfissionalService profissionalService;
-
 	public ProfissionalAPIConsumer() {
 		client = new OkHttpClient().newBuilder().build();
 		objectMapper = new ObjectMapper();
 	}
 
-	public void consumir() {
+	public List<Profissional> consumir() {
 		Request request = new Request.Builder().url(RESOURCEURL).method("GET", null).build();
 		try (Response response = client.newCall(request).execute()) {
 
 			JsonNode rootNode = objectMapper.readTree(response.body().bytes());
 			JsonNode contentNode = rootNode.path("content");
 
-			contentNode.forEach(p -> {
-				try {
-					Profissional profissional = objectMapper.treeToValue(p, Profissional.class);
-					salvarProfissionalNoBanco(profissional);
-				} catch (JsonProcessingException e) {
-					System.err.println("Erro na conversão de Profissional: " + e.getMessage());
-				}
-			});
+			List<Profissional> listaProfissionais = Arrays
+					.asList(objectMapper.treeToValue(contentNode, Profissional[].class));
+
+			return listaProfissionais;
 
 		} catch (IOException e) {
-			System.err.println("Erro na busca pela API de profissionais: " + e.getMessage());
+			e.printStackTrace();
+			return null;
 		}
-	}
-
-	private void salvarProfissionalNoBanco(Profissional profissional) {
-		profissionalService.salvar(profissional);
 	}
 
 }
--- backend/src/main/java/com/elotech/service/ProfissionalService.java
+++ backend/src/main/java/com/elotech/service/ProfissionalService.java
@@ -2,24 +2,19 @@ package com.elotech.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.elotech.client.consumer.ProfissionalAPIConsumer;
 import com.elotech.model.Profissional;
-import com.elotech.repository.ProfissionalRepository;
 
 @Service
 public class ProfissionalService {
 
-	@Autowired
-	private ProfissionalRepository profissionalRepository;
-
 	public List<Profissional> listar() {
-		return profissionalRepository.findAll();
-	}
 
-	public void salvar(Profissional profissional) {
-		profissionalRepository.save(profissional);
+		ProfissionalAPIConsumer consumer = new ProfissionalAPIConsumer();
+		return consumer.consumir();
+
 	}
 
 }
